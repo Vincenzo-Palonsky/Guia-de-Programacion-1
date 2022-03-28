2515c587ddad10b9b05402cda4e871d2823eff1e
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-03-28 11:40:26 -0300

    Exercises 1 to 5 done

---
+++ Assets/Scripts/Ejercicio_1.cs
@@ -0,0 +1,20 @@
+﻿//1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0.
+//Luego incrementá su valor en 2 y mostralo por pantalla. 
+//Después mostrá el  resultado de multiplicarlo por sí mismo.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 0;
+        num1 += 2;
+
+        Debug.Log(num1);
+        Debug.Log((num1 *= num1));
+    }
+}
---
+++ Assets/Scripts/Ejercicio_2.cs
@@ -0,0 +1,22 @@
+﻿//2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5, 
+//respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+//a.Suma
+//b.Producto
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        
+        Debug.Log("a. " + (num1 + num2));
+        Debug.Log("b. " + (num1 * num2));
+    }
+}
