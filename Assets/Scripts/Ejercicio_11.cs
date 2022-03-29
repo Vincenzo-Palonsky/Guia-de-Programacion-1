de3a420df676e45537f363b914131ee5a8a41409
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-03-29 14:29:08 -0300

    Exercise 11 started

--- Assets/Scripts/Ejercicio_11.cs
+++ Assets/Scripts/Ejercicio_11.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿//Realizá un programa que resuelva el siguiente problema:  
+//Deberá ingresarse por Inspector una fecha de compra(String en el siguiente formato:  YYYYMMDD),
+//un nombre de comprador, un nombre de producto y una cantidad y precio del producto comprado.
+//Mostrá a modo de ticket, la información ingresada y el monto a pagar.Modelo de Ticket:  
+//Fecha de Compra: YYYYMMDD
+//Nombre del Comprador: xxxxx xxxxx
+//Producto solicitado: xxxxx
+//Cantidad solicitada: xx
+//Precio Unitario: $xxx
+//Total a Pagar: $xxxxx
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio_11 : MonoBehaviour
 {
+    public string FechaCompra;
+    public string NombreComprador;
+    public string NombreProducto;
+    public int CantidadProducto;
+    public int PrecioProducto;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Fecha de Compra: " + FechaCompra + "\n");
     }
 }
