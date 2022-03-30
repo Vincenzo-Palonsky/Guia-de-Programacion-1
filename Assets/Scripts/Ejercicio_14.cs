d6f6f7b4ca40ed41525092609f03f1e066629003
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-03-30 09:51:10 -0300

    Exercise 14 finished and 15 started

--- Assets/Scripts/Ejercicio_14.cs
+++ Assets/Scripts/Ejercicio_14.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿//Construir un programa que simule el funcionamiento de una calculadora que puede realizar las cuatro
+//operaciones aritméticas básicas (suma, resta, producto y división) con  valores numéricos enteros.  
+//Se debe especificar la operación que se desea realizar ingresando una letra por Inspector
+//(s para la suma, r para la resta, p para el producto y d para la división) y luego, 
+//deberán ingresarse dos números enteros para así realizar la operación. Informar el resultado por pantalla.
+//Nota: Se recomienda el empleo de una sentencia switch.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio_14 : MonoBehaviour
 {
+    public string Operacion;
+    public int Numero1;
+    public int Numero2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        switch (Operacion)
+        {
+            case "s":
+                Debug.Log("El resultado de la suma es: " + (Numero1 + Numero2));
+                break;
 
-    // Update is called once per frame
-    void Update()
-    {
+            case "r":
+                Debug.Log("El resultado de la resta es: " + (Numero1 - Numero2));
+                break;
+
+            case "p":
+                Debug.Log("El resultado del producto es: " + (Numero1 * Numero2));
+                break;
+
+            case "d":
+                Debug.Log("El resultado de la división es: " + (Numero1 / Numero2));
+                break;
+        }
         
     }
 }
--- Assets/Scripts/Ejercicio_15.cs
+++ Assets/Scripts/Ejercicio_15.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿//Existen dos reglas que identifican dos conjuntos de valores: 
+//- El número es de un solo dígito.
+//- El número es impar.
+//A partir de estas reglas, creá un algoritmo que asigne a las variables booleanas
+//estaEnA, estaEnB, estaEnAmbos y noEstaEnNinguno el valor verdadero o falso, según corresponda,
+//para indicar si el valor ingresado pertenece al primer conjunto, al segundo, a ambos o a ninguno, respectivamente.
+//Definí un lote de prueba de varios números y probá el algoritmo, escribiendo los resultados.
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio_15 : MonoBehaviour
 {
+    public int Numero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        bool estaEnA;
+        bool estaEnB;
+        bool estaEnAmbos;
+        bool noEstaEnNinguno;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Numero < 10 && Numero > -10)
+        {
+            estaEnA = true;
+        }
+
+        if (Numero % 2 != 0)
+        {
+            estaEnB = true;
+        }
     }
 }
